e588bf87e924a9639119817fcb4ff7c0ceb520db
Michael Lin <linzichunzf@hotmail.com>
2017-02-10 00:22:47 -0700

    import data model

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -3,4 +3,7 @@ from .library import controller as library_api
 from .list import controller as list_api
 from .user import controller as user_api
 from .user.model import User
+from .list.model import List
+from .article.model import Article
+from .library.model import List
 
