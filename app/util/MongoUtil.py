c5a4ab727dbdb4fe9517cf1ec729f095f0b6a873
Michael Lin <linzichunzf@hotmail.com>
2017-02-10 00:22:12 -0700

    add serval util method

--- app/util/MongoUtil.py
+++ app/util/MongoUtil.py
@@ -1,4 +1,6 @@
 from app.api.user.model import User
+from app.api.article.model import Article
+from app.api.library.model import List
 
 
 def find_user(email):
@@ -9,6 +11,38 @@ def find_user(email):
 
 
 def create_user(email):
-    user = User(email=email).save()
-    return user
+    # Create new user
+    new_user = User(email=email).save()
+
+    return new_user
+
+
+def find_list(list_id):
+    # Find user's list
+    user_list = List.objects(id=list_id)
+
+    # Check if exists
+    if List.objects(id__exact=list_id).count() == 0:
+        return False
+
+    return user_list
+
+
+def create_list(list_name):
+    # Create new list
+    new_list = List(name=list_name).save()
+
+    return new_list
+
+
+def create_article(title, list_id, tags=None):
+    # Create new article
+    new_article = Article(title=title).save()
+
+    # Add article the user's list
+    List.objects(id=list_id).update_one(push__articles=new_article).save()
+
+    return new_article
+
+
 
