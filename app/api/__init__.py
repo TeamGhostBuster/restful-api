7ba33a5a9c83ddb86fe6f5a867320021f4c5aef5
Michael Lin <linzichunzf@hotmail.com>
2017-02-10 19:44:39 -0700

    fix import error

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,8 +2,9 @@ from .article import controller as article_api
 from .library import controller as library_api
 from .list import controller as list_api
 from .user import controller as user_api
-from .user.model import User
-from .list.model import List
-from .article.model import Article
-from .library.model import List
-
+from .group import controller as group_api
+from .article import model as article_model
+from .library import model as library_model
+from .list import model as list_model
+from .user import model as user_model
+from .group import model as group_model
